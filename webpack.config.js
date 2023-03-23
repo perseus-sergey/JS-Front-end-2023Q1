862bfb7800534af911ba9090aae169c40a9d2842
Sergey Gubriy <sergeygubriy@gmail.com>
2023-03-23 02:26:47 +0200

    init: start project & create basic files (2023-03-23 02:26:47)

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'shelter'),
+        filename: 'index.[contenthash:8].js',
+        assetModuleFilename: path.join('assets/images', '[name].[contenthash][ext]'),
+    },
+    performance: {
+        hints: false,
+        maxAssetSize: 512000,
+        maxEntrypointSize: 512000
+    },
+    devServer: {
+        port: 9000,
+        compress: true,
+        hot: true,
+        static: {
+            directory: path.join(__dirname, 'shelter')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(css|scss|sass)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('assets/svg', '[name].[contenthash][ext]'),
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'template.html'),
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: true,
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'temp_pets.html'),
+            filename: 'pets/index.html',
+            minify: {
+                collapseWhitespace: false,
+            }
+        }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['shelter'],
+                },
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+};
